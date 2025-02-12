450592958a14e58942eb4bfd70f601d215c981c6
Teo Wei Chuen <Teo Wei Chuen@DESKSTOP-HANQIN>
2025-02-12 13:59:49 +0800

    Add project files.

---
+++ Testing/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Testing.Controllers
+{
+    public class UserController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+    
+            // Create a sample User object
+            var user = new User
+            {
+                Id = 1,
+                Name = "Xin Jun",
+                Email = "xin@example.com"
+            };
+           
+            return View(user); // ✅ Pass model to the view
+            // Pass values to View
+           
+           
+        }
+    }
+}
---
+++ Testing/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Testing.Models
+{
+
+   public class User
+   {
+        [DisplayName("User ID")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+   }
+  
+}
