c673e2ed59783dcc8f521ce02e5d3cabb2d6c4b0
wenbin <wenbinchong1124@gmail.com>
2025-02-13 15:18:43 +0800

    Profile

--- Testing/Controllers/HomeController.cs
+++ Testing/Controllers/HomeController.cs
@@ -35,6 +35,12 @@ namespace Testing.Controllers
             return View();
         }
 
+        public IActionResult Profile()
+        {
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
